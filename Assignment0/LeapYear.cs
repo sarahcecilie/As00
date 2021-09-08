b130fb2e73875e4343ff2ddcef5a75429b1034c4
sacc <sacc@itu.dk>
2021-09-08 08:27:33 +0200

    user interface for isLeapYear has been created

--- Assignment0/LeapYear.cs
+++ Assignment0/LeapYear.cs
@@ -5,19 +5,24 @@ namespace doAssignment0
     public class LeapYear
     {
         public static void Main(string[] args){
-           Console.WriteLine("Hello world!");
+           int input = Convert.ToInt32(Console.ReadLine());
+            if(isLeapYear(input) == true){
+                Console.WriteLine("yay");
+            } else{
+                Console.WriteLine("nay");
+            }
         }
-           public bool isLeapYear(int year){
-                if(year % 4 == 0){
-                    if(year % 400 == 0){
-                        return true;
-                    } if(year % 100 == 0){
-                        return false;
-                    }
+        public static bool isLeapYear(int year){
+            if(year % 4 == 0){
+                if(year % 400 == 0){
                     return true;
-                } else{
+                } if(year % 100 == 0){
                     return false;
                 }
+                return true;
+            } else{
+                return false;
             }
+        }
     }
 }
