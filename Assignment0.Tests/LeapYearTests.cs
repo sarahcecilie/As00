ffaab022406ea8cce4e09730c9b639f8b239279b
sarahcecilie <90042239+sarahcecilie@users.noreply.github.com>
2021-09-08 08:35:50 +0200

    Feature/leapyear (#3)
    * Setup project
    
    * IsLeapYear implemented + tested
    
    * IsLeapYear implemented pt. 2
    
    * deleted something
    
    * user interface for isLeapYear has been created
    
    Co-authored-by: sacc <sacc@itu.dk>

--- Assignment0.Tests/LeapYearTests.cs
+++ Assignment0.Tests/LeapYearTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 using doAssignment0;
 
@@ -7,37 +8,68 @@ namespace Assignment0.Tests
     public class LeapYearTests
     {
         [Fact]
-        public void leapYear_is_true_4()
+
+        public void leapYear_is_true_when_divided_by_4()
         {
-            var leapYear = new LeapYear();
             var output = false;
 
-            output = leapYear.isLeapYear(2000);
+            output = LeapYear.isLeapYear(1992);
 
             Assert.True(output);
         }
 
         [Fact]
-        public void leapYear_is_false_100()
+
+        public void leapYear_is_false_when_divided_by_100()
         {
-            var leapYear = new LeapYear();
             var output = false;
 
-            output = leapYear.isLeapYear(1900);
+            output = LeapYear.isLeapYear(1900);
 
             Assert.False(output);
         }
 
         [Fact]
-        public void leapYear_is_true_400()
+      
+        public void leapYear_is_true_when_divided_by_400()
         {
-            var leapYear = new LeapYear();
             var output = false;
 
-            output = leapYear.isLeapYear(1600);
+            output = LeapYear.isLeapYear(2000);
 
             Assert.True(output);
         }
 
+        [Fact]
+
+        public void leapYear_prints_yay()
+        {
+            var input = new StringReader("1600");
+            Console.SetIn(input);
+
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            LeapYear.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("yay", output);
+
+        }
+
+        [Fact]
+        public void leapYear_prints_nay()
+        {
+            var input = new StringReader("1800");
+            Console.SetIn(input);
+
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            LeapYear.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("nay", output);
+        }
     }
 }
